f48d41b195de186cab0c32db440561d2b2b7eecd
Marc Qualie <marc@marcqualie.com>
2019-08-07 08:35:11 +0100

    Add google analytics in production

---
+++ pages/_app.tsx
@@ -0,0 +1,43 @@
+import App, { Container, AppContext } from 'next/app'
+import Router from 'next/router'
+import React from 'react'
+
+declare global {
+  interface Window {
+    ga: (action: 'send', type: 'pageview', path: string) => void
+  }
+}
+
+interface Props {
+  Component: React.Component
+  pageProps: object
+}
+
+export default class MyApp extends App<Props> {
+  static async getInitialProps({ Component, ctx }: AppContext) {
+    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {}
+
+    return {
+      pageProps,
+    }
+  }
+
+  componentDidMount() {
+    Router.events.on('routeChangeComplete', url => {
+      console.log('$pageview', url)
+      if (window.ga) {
+        window.ga('send', 'pageview', url)
+      }
+    })
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Component {...pageProps}/>
+      </Container>
+    )
+  }
+}
