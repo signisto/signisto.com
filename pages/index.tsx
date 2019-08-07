90fe31abd2797375d0b8dc691b2fe0b752aee576
Marc Qualie <marc@marcqualie.com>
2019-08-07 08:40:04 +0100

    Use local logo instead of absolute URL

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Layout from '../components/Layout'
 const Page: NextPage = () => (
   <Layout navbar={false}>
     <div className="viewport-stretch bg-primary bg-bitmap">
-      <img src="https://signisto.com/assets/logo-light-shadow-bd20ce7b42cf32c0d58d7869afd1fe84ee4f1248f03fb5b7c0efc739b20ac668.svg" alt="Signisto Logo" className="logo-main"/>
+      <img src="/static/images/logo-light-shadow.svg" alt="Signisto Logo" className="logo-main"/>
 
       <h1 className="viewport-stretch-title">
         Software Development &amp; Consulting
